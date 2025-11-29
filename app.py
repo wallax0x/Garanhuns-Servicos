3a4758533abee91710e279529bf5256173114232
Maurício <75014760+wallax0x@users.noreply.github.com>
2025-11-29 16:32:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -510,6 +510,9 @@ def criar_admin_padrao():
 
         
 
-if __name__ == '__main__':
-    criar_admin_padrao()  # Executa a verificação antes de iniciar o servidor
-    app.run(debug=True)
+if __name__ == "__main__":
+    criar_admin_padrao()
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
